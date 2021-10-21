c9faca4b20c234a050f366938bd56798af88581b
Justine Ventura <jvent@venturazure.onmicrosoft.com>
2021-10-21 12:58:06 -0600

    add to createhabitevent activity

--- src/app/src/main/java/com/cmput301f21t09/budgetprojectname/CreateHabitEvent.java
+++ src/app/src/main/java/com/cmput301f21t09/budgetprojectname/CreateHabitEvent.java
@@ -3,12 +3,33 @@ package com.cmput301f21t09.budgetprojectname;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.DatePicker;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class CreateHabitEvent extends AppCompatActivity {
+
+    private TextView habitEventName;
+    private EditText location;
+    private EditText description;
+    private DatePicker date;
+    private ImageView image;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_habit_event);
+
+        // TODO: get ID from Intent (from HabitListActivity)
+
+        // TODO: query ID in Firestore, populate this field with habitName
+        habitEventName = (TextView) findViewById(R.id.habiteventName);
+        location = (EditText) findViewById(R.id.location);
+        description = (EditText) findViewById(R.id.description);
+        date = (DatePicker) findViewById(R.id.datePicker);
+        image = (ImageView) findViewById(R.id.image);
+
+        // TODO: add onclick event
     }
 }
--- src/app/src/main/java/com/cmput301f21t09/budgetprojectname/MainActivity.java
+++ src/app/src/main/java/com/cmput301f21t09/budgetprojectname/MainActivity.java
@@ -10,6 +10,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         // setContentView(R.layout.activity_main);
+        // Moved to main activity for testing purposes:
         setContentView(R.layout.activity_create_habit_event);
     }
 }

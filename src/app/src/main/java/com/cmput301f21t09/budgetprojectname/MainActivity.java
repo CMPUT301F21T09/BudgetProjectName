7fe3001e7cb284daaf4b981eafe4bc1bffb939fd
cheetiantey <37930535+cheetiantey@users.noreply.github.com>
2021-10-26 17:39:37 -0600

    Add a makeshift button that mimics bringing the user from Habit List screen to View Habit Details screen

--- src/app/src/main/java/com/cmput301f21t09/budgetprojectname/MainActivity.java
+++ src/app/src/main/java/com/cmput301f21t09/budgetprojectname/MainActivity.java
@@ -2,7 +2,11 @@ package com.cmput301f21t09.budgetprojectname;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // TODO: Move this function to relate to each habit that a user has and pass habitID to intent
+        final Button habitEventBtn = findViewById(R.id.viewHabitDetailsBtn);
+        habitEventBtn.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), ViewHabitDetails.class);
+                final String HABIT_ID = "HABIT_ID";
+                int habitid = 1;
+                intent.putExtra(HABIT_ID, habitid);
+                startActivity(intent);
+            }
+        });
     }
 }

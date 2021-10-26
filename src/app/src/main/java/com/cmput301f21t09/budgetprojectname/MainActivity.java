02000ee29472be18088a26406440cc573e594382
Junesung Lee <junesoung412@gmail.com>
2021-10-25 19:23:10 -0600

    Set Up Bottom Navigation Bar and Fragments

--- src/app/src/main/java/com/cmput301f21t09/budgetprojectname/MainActivity.java
+++ src/app/src/main/java/com/cmput301f21t09/budgetprojectname/MainActivity.java
@@ -1,14 +1,44 @@
 package com.cmput301f21t09.budgetprojectname;
 
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
 
+    @SuppressLint("NonConstantResourceId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        MainFragment mainFragment = new MainFragment();
+        AddFragment addFragment = new AddFragment();
+        SearchFragment searchFragment = new SearchFragment();
+        ProfileFragment profileFragment = new ProfileFragment();
+
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_nav);
+        bottomNavigationView.setOnItemSelectedListener(item -> {
+            switch (item.getItemId()) {
+                case R.id.main:
+                    getSupportFragmentManager().beginTransaction().replace(R.id.fragment, mainFragment).commit();
+                    return true;
+                case R.id.add:
+                    getSupportFragmentManager().beginTransaction().replace(R.id.fragment, addFragment).commit();
+                    return true;
+                case R.id.search:
+                    getSupportFragmentManager().beginTransaction().replace(R.id.fragment, searchFragment).commit();
+                    return true;
+                case R.id.profile:
+                    getSupportFragmentManager().beginTransaction().replace(R.id.fragment, profileFragment).commit();
+                    return true;
+            }
+            return false;
+        });
+        bottomNavigationView.setSelectedItemId(R.id.main);
+
     }
 }

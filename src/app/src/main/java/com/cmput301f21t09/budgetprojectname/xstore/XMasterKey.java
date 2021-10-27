3c8d35f9bdb56f5e1bc7755128781046080b1be6
Mac Malainey <mmalaine@ualberta.ca>
2021-10-27 11:48:02 -0600

    Add initial state management solution (#83)

---
+++ src/app/src/main/java/com/cmput301f21t09/budgetprojectname/xstore/XMasterKey.java
@@ -0,0 +1,27 @@
+package com.cmput301f21t09.budgetprojectname.xstore;
+
+/**
+ * Key for tracking the state of an immutable object
+ * @param <T> Data Type - Should be immutable
+ */
+public class XMasterKey<T> extends XKey<T>{
+
+    /**
+     * Internal state
+     */
+    private T state;
+
+    @Override
+    protected T getState() {
+        return state;
+    }
+
+    @Override
+    protected boolean onUpdate(T newState) {
+        // Update state if the new state is different from the old one
+        if (state.equals(newState)) return false;
+
+        state = newState;
+        return true;
+    }
+}

68f421f8798e687061b1d8790257e400b99c6a92
Junesung Lee <junesoung412@gmail.com>
2021-10-28 23:59:02 -0600

    Rafctoring Define Habit Event UI

--- src/app/src/main/java/com/cmput301f21t09/budgetprojectname/MainActivity.java
+++ src/app/src/main/java/com/cmput301f21t09/budgetprojectname/MainActivity.java
@@ -1,11 +1,8 @@
 package com.cmput301f21t09.budgetprojectname;
 
-import androidx.appcompat.app.AppCompatActivity;
-
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 

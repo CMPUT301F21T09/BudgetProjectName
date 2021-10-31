7bc2029db504b6ad04176829b020e8d486d19176
Junesung Lee <junesoung412@gmail.com>
2021-10-31 16:30:29 -0600

    DefineHabitEventActivity: Add 'Add Image' Feature

--- src/app/src/main/java/com/cmput301f21t09/budgetprojectname/DefineHabitEventActivity.java
+++ src/app/src/main/java/com/cmput301f21t09/budgetprojectname/DefineHabitEventActivity.java
@@ -1,24 +1,40 @@
 package com.cmput301f21t09.budgetprojectname;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.app.Activity;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
 import android.view.View;
-import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.FileProvider;
+
+import com.google.android.material.dialog.MaterialAlertDialogBuilder;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.util.Date;
 
 public class DefineHabitEventActivity extends AppCompatActivity {
 
-    private TextView screenTitle;
+    private TextView toolbarTitle;
     private TextView habitEventName;
     private EditText location;
     private EditText description;
     private ImageView image;
+
+    String mPhotoPath;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,17 +53,17 @@ public class DefineHabitEventActivity extends AppCompatActivity {
         }
 
         // update title according to mode selected: "add" or "edit"
-        screenTitle = (TextView) findViewById(R.id.CreateEditHabitEventTitle);
-        screenTitle.setText(modeStr);
+        //  toolbarTitle = findViewById(R.id.toolbar_title);
+        //  toolbarTitle.setText(modeStr);
 
         // TODO: query habitID in Firestore and populate this field with corresponding habitName
-        habitEventName = (TextView) findViewById(R.id.habiteventName);
-        location = (EditText) findViewById(R.id.location);
-        description = (EditText) findViewById(R.id.description);
-        image = (ImageView) findViewById(R.id.image);
+        habitEventName = findViewById(R.id.habitName);
+        location = findViewById(R.id.location);
+        description = findViewById(R.id.description);
+        image = findViewById(R.id.image);
 
-        final Button checkBtn = findViewById(R.id.checkBtn);
-        checkBtn.setOnClickListener(new View.OnClickListener() {
+        ImageButton doneBtn = findViewById(R.id.done);
+        doneBtn.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
                 String nameStr = habitEventName.getText().toString();
                 String locationStr = location.getText().toString();
@@ -58,5 +74,79 @@ public class DefineHabitEventActivity extends AppCompatActivity {
                 // TODO: save HabitEvent in Firestore DB
             }
         });
+
+        //Get Image from Launcher
+        ActivityResultLauncher<Intent> GalleryResultLauncher = registerForActivityResult(
+                new ActivityResultContracts.StartActivityForResult(),
+                result -> {
+                    if (result.getResultCode() == Activity.RESULT_OK) {
+                        Uri selectedImage = result.getData().getData();
+                        try {
+                            Bitmap bitmap = BitmapFactory.decodeStream(getBaseContext().getContentResolver().openInputStream(selectedImage));
+                            image.setImageBitmap(bitmap);
+                        } catch (FileNotFoundException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                });
+
+        //Get Image from Camera
+        ActivityResultLauncher<Intent> CameraResultLauncher = registerForActivityResult(
+                new ActivityResultContracts.StartActivityForResult(),
+                result -> {
+                    if (result.getResultCode() == Activity.RESULT_OK) {
+                        File file = new File(mPhotoPath);
+                        Bitmap selectedImage = null;
+                        try {
+                            selectedImage = MediaStore.Images.Media.getBitmap(getContentResolver(), Uri.fromFile(file));
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+                        image.setImageBitmap(selectedImage);
+                    }
+                });
+
+        String[] actionList = new String[]{"Gallery", "Take a Photo"};
+
+        image.setOnClickListener(v -> new MaterialAlertDialogBuilder(this, R.style.MyDialogTheme)
+                .setTitle("Select Image From")
+                .setItems(actionList, (dialog, which) -> {
+                    if (which == 0) {
+                        Intent galleryIntent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI).setType("image/*");
+                        if (galleryIntent.resolveActivity(getPackageManager()) != null) {
+                            GalleryResultLauncher.launch(galleryIntent);
+                        }
+                    } else if (which == 1) {
+                        Intent cameraIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                        if (cameraIntent.resolveActivity(getPackageManager()) != null) {
+                            // Create the File where the photo should go
+                            File photoFile = null;
+                            try {
+                                photoFile = createImageFile();
+                            } catch (IOException ex) {
+                                // Error occurred while creating the File
+                            }
+                            // Continue only if the File created successfully
+                            if (photoFile != null) {
+                                Uri photoURI = FileProvider.getUriForFile(this,
+                                        "com.cmput301f21t09.budgetprojectname.provider",
+                                        photoFile);
+                                cameraIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI);
+                                CameraResultLauncher.launch(cameraIntent);
+                            }
+                        }
+                    }
+                }).show());
     }
+
+    private File createImageFile() throws IOException {
+        // Create an image file name
+        String imageFileName = "temp";
+        File storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+        File image = File.createTempFile(imageFileName, ".jpg", storageDir);
+
+        mPhotoPath = image.getAbsolutePath();
+        return image;
+    }
+
 }
--- src/app/src/main/java/com/cmput301f21t09/budgetprojectname/ExampleActivity.java
+++ src/app/src/main/java/com/cmput301f21t09/budgetprojectname/ExampleActivity.java
@@ -1,8 +1,11 @@
 package com.cmput301f21t09.budgetprojectname;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -19,6 +22,10 @@ public class ExampleActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_example);
 
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA) == PackageManager.PERMISSION_DENIED) {
+            requestPermissions(new String[]{Manifest.permission.CAMERA, Manifest.permission.READ_EXTERNAL_STORAGE}, 1);
+        }
+
         // TODO: move button into habitlist activity and pass habitID to intent
         final Button habitEventBtn = findViewById(R.id.createhabiteventBtn);
         habitEventBtn.setOnClickListener(new View.OnClickListener() {
@@ -32,5 +39,29 @@ public class ExampleActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions,
+                                           int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        switch (requestCode) {
+            case 1:
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.length > 0 &&
+                        grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    // Permission is granted. Continue the action or workflow
+                    // in your app.
+                } else {
+                    // Explain to the user that the feature is unavailable because
+                    // the features requires a permission that the user has denied.
+                    // At the same time, respect the user's decision. Don't link to
+                    // system settings in an effort to convince the user to change
+                    // their decision.
+                }
+                return;
+        }
+        // Other 'case' lines to check for other
+        // permissions this app might request.
     }
 }

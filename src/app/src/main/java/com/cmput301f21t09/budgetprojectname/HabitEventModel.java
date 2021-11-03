49534464ccf074ac1eeb3ae8821eecac45043fa3
Junesung Lee <junesoung412@gmail.com>
2021-11-02 22:27:48 -0600

    HabitEventModel: Change image variable type

--- src/app/src/main/java/com/cmput301f21t09/budgetprojectname/HabitEventModel.java
+++ src/app/src/main/java/com/cmput301f21t09/budgetprojectname/HabitEventModel.java
@@ -1,22 +1,18 @@
 package com.cmput301f21t09.budgetprojectname;
 
+import android.graphics.Bitmap;
 import android.media.Image;
-import android.widget.DatePicker;
-import android.widget.EditText;
-import android.widget.ImageView;
-import android.widget.TextView;
 
-import java.io.Serializable;
 import java.util.Date;
 
-public class HabitEventModel{
+public class HabitEventModel {
     private final String ID; // TODO: change type depending on Firebase ID type
     private final String location; // TODO: change to Location once this is implemented
     private final String description;
     private final Date date;
-    private final Image image;
+    private final Bitmap image;
 
-    HabitEventModel(String location, Date date, String description, Image image) {
+    HabitEventModel(String location, Date date, String description, Bitmap image) {
         this.ID = "s1w9kwequ290"; // placeholder ID
         this.location = location;
         this.description = description;
@@ -32,7 +28,7 @@ public class HabitEventModel{
         image = null;
     }
 
-    HabitEventModel(String location, Date date, Image image) {
+    HabitEventModel(String location, Date date, Bitmap image) {
         this.ID = "s1w9kwequ290"; // placeholder ID
         this.location = location;
         this.date = date;
@@ -40,23 +36,23 @@ public class HabitEventModel{
         this.description = null;
     }
 
-    public String getID(){
+    public String getID() {
         return ID;
     }
 
-    public String getLocation(){
+    public String getLocation() {
         return location;
     }
 
-    public String getDescription(){
+    public String getDescription() {
         return description;
     }
 
-    public Date getDate(){
+    public Date getDate() {
         return date;
     }
 
-    public Image getImage(){
+    public Bitmap getImage() {
         return image;
     }
 

ccc1da9270a12b8e9fa9f5a89cbf842a4607a3d8
Junesung Lee <junesoung412@gmail.com>
2021-11-03 20:58:53 -0600

    set today date:

--- src/app/src/main/java/com/cmput301f21t09/budgetprojectname/DailyHabitFragment.java
+++ src/app/src/main/java/com/cmput301f21t09/budgetprojectname/DailyHabitFragment.java
@@ -1,5 +1,6 @@
 package com.cmput301f21t09.budgetprojectname;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -7,11 +8,13 @@ import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.Spinner;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
 
@@ -20,12 +23,17 @@ public class DailyHabitFragment extends Fragment {
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_main, container, false);
+        return inflater.inflate(R.layout.fragment_daily_habit, container, false);
     }
 
+    @SuppressLint("SimpleDateFormat")
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+
+        // Set Today's Date
+        TextView dateText = view.findViewById(R.id.today_date);
+        dateText.setText(new SimpleDateFormat("EEEE, MMMM d").format(new Date()));
 
         // Spinner Setup
         ArrayList<String> arrayList = new ArrayList<>();

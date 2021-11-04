d76dcd9318863160ef3f4ce0bb20e2ec20568bc8
Junesung Lee <junesoung412@gmail.com>
2021-11-03 18:16:15 -0600

    Temporary

--- src/app/src/main/java/com/cmput301f21t09/budgetprojectname/CurrentUserProfileFragment.java
+++ src/app/src/main/java/com/cmput301f21t09/budgetprojectname/CurrentUserProfileFragment.java
@@ -9,11 +9,16 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
-public class ProfileFragment extends Fragment {
+public class CurrentUserProfileFragment extends Fragment {
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_profile, container, false);
+        return inflater.inflate(R.layout.fragment_current_user_profile, container, false);
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
     }
 }

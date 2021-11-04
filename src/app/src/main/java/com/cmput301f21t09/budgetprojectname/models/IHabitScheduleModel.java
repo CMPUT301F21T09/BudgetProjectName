224de065e9eda6aadf6016da8a5212869514c45d
Mac Malainey <mmalaine@ualberta.ca>
2021-11-04 00:50:32 -0600

    Add Habit Schedule Model (#127)
    * Add Habit Schedule Model
    
    * code cleanup: whitespace

---
+++ src/app/src/main/java/com/cmput301f21t09/budgetprojectname/models/IHabitScheduleModel.java
@@ -0,0 +1,30 @@
+package com.cmput301f21t09.budgetprojectname.models;
+
+import java.util.Date;
+import java.util.Map;
+
+/**
+ * Model interface for a habit's schedule
+ */
+public interface IHabitScheduleModel {
+
+    /**
+     * Check if the habit is meant to be completed on the given date
+     * @param date to check
+     * @return true if the habit should be completed on the given date
+     */
+    boolean isToBeCompletedOn(Date date);
+
+    /**
+     * Check if the are missing days if the habit was last completed on the given date
+     * @param date to check
+     * @return true if there are missing days in the schedule
+     */
+    boolean wasSkippedIfLastCompletedOn(Date date);
+
+    /**
+     * Serialize the model to a map
+     * @return serialized version of model
+     */
+    Map<String, Object> toMap();
+}

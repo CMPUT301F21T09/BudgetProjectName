5c9b9d0337b3526830853fdd3e710f145790a8f2
cheetiantey <37930535+cheetiantey@users.noreply.github.com>
2021-11-03 21:29:37 -0600

    Fix HabitEventCustomList

--- src/app/src/main/java/com/cmput301f21t09/budgetprojectname/HabitEventCustomList.java
+++ src/app/src/main/java/com/cmput301f21t09/budgetprojectname/HabitEventCustomList.java
@@ -1,5 +1,7 @@
 package com.cmput301f21t09.budgetprojectname;
 
+import static java.util.Objects.isNull;
+
 import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -14,13 +16,13 @@ import java.util.ArrayList;
 import java.util.Date;
 
 public class HabitEventCustomList extends ArrayAdapter<HabitEventModel> {
-    private ArrayList<HabitEventModel> habits;
+    private ArrayList<HabitEventModel> habitEvents;
     private Context context;
 
 
-    public HabitEventCustomList(Context context, ArrayList<HabitEventModel> habits) {
-        super(context, 0, habits);
-        this.habits = habits;
+    public HabitEventCustomList(Context context, ArrayList<HabitEventModel> habitEvents) {
+        super(context, 0, habitEvents);
+        this.habitEvents = habitEvents;
         this.context = context;
     }
 
@@ -30,6 +32,8 @@ public class HabitEventCustomList extends ArrayAdapter<HabitEventModel> {
             view = LayoutInflater.from(context).inflate(R.layout.activity_view_habit_event_custom_list, parent, false);
         }
 
+        HabitEventModel habitEvent = habitEvents.get(position);
+
         TextView habitDate = view.findViewById(R.id.date);
         ImageView locationIcon = view.findViewById(R.id.location_icon);
         ImageView descriptionIcon = view.findViewById(R.id.comment_icon);
@@ -37,12 +41,27 @@ public class HabitEventCustomList extends ArrayAdapter<HabitEventModel> {
 
         // Todo: Change the date of past habit events to actual habit event data from Firestore
         DateFormat df = new SimpleDateFormat("MMMM dd, YYYY");
-        habitDate.setText(df.format(new Date()));
+        habitDate.setText(df.format(habitEvent.getDate()));
 
         // Todo: Change the icons' opacity according to the actual habit events' data from Firestore
-        locationIcon.setImageResource(R.drawable.ic_map_marker_positive);
-        descriptionIcon.setImageResource(R.drawable.ic_comment_negative);
-        ImageIcon.setImageResource(R.drawable.ic_image_positive);
+        if (isNull(habitEvent.getLocation())) {
+            locationIcon.setImageResource(R.drawable.ic_map_marker_negative);
+        } else {
+            locationIcon.setImageResource(R.drawable.ic_map_marker_positive);
+        }
+        if (isNull(habitEvent.getComment())) {
+            descriptionIcon.setImageResource(R.drawable.ic_comment_negative);
+        } else {
+            descriptionIcon.setImageResource(R.drawable.ic_comment_positive);
+        }
+        if (isNull(habitEvent.getImage())) {
+            ImageIcon.setImageResource(R.drawable.ic_image_negative);
+        } else {
+            ImageIcon.setImageResource(R.drawable.ic_image_positive);
+        }
+//        locationIcon.setImageResource(R.drawable.ic_map_marker_positive);
+//        descriptionIcon.setImageResource(R.drawable.ic_comment_negative);
+//        ImageIcon.setImageResource(R.drawable.ic_image_positive);
 
         return view;
     }

7f3371b7faa90e7eb2670b8ed3c214f39aed7afb
cheetiantey <37930535+cheetiantey@users.noreply.github.com>
2021-11-04 04:07:37 -0600

    Add comments and fixes

--- src/app/src/main/java/com/cmput301f21t09/budgetprojectname/HabitEventCustomList.java
+++ src/app/src/main/java/com/cmput301f21t09/budgetprojectname/HabitEventCustomList.java
@@ -1,5 +1,7 @@
 package com.cmput301f21t09.budgetprojectname;
 
+import static java.util.Objects.isNull;
+
 import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -11,16 +13,23 @@ import androidx.annotation.Nullable;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
-import java.util.Date;
 
+/**
+ * CustomList for ViewHabitActivity
+ * Show the habit event's date and whether or not the user has added a location, comment, and/or photograph
+ */
 public class HabitEventCustomList extends ArrayAdapter<HabitEventModel> {
-    private ArrayList<HabitEventModel> habits;
+    private ArrayList<HabitEventModel> habitEvents;
     private Context context;
 
-
-    public HabitEventCustomList(Context context, ArrayList<HabitEventModel> habits) {
-        super(context, 0, habits);
-        this.habits = habits;
+    /**
+     * Constructor for HabitEventCustomList
+     * @param context
+     * @param habitEvents
+     */
+    public HabitEventCustomList(Context context, ArrayList<HabitEventModel> habitEvents) {
+        super(context, 0, habitEvents);
+        this.habitEvents = habitEvents;
         this.context = context;
     }
 
@@ -30,19 +39,50 @@ public class HabitEventCustomList extends ArrayAdapter<HabitEventModel> {
             view = LayoutInflater.from(context).inflate(R.layout.activity_view_habit_event_custom_list, parent, false);
         }
 
+        HabitEventModel habitEvent = habitEvents.get(position);
+
+        /* Views */
+
+        /**
+         * Text View for habit date
+         */
         TextView habitDate = view.findViewById(R.id.date);
+
+        /**
+         * Image View for location icon
+         */
         ImageView locationIcon = view.findViewById(R.id.location_icon);
+
+        /**
+         * Image View for description icon
+         */
         ImageView descriptionIcon = view.findViewById(R.id.comment_icon);
+
+        /**
+         * Image View for Image icon
+         */
         ImageView ImageIcon = view.findViewById(R.id.image_icon);
 
-        // Todo: Change the date of past habit events to actual habit event data from Firestore
+        // Set the date when the habit event occured
         DateFormat df = new SimpleDateFormat("MMMM dd, YYYY");
-        habitDate.setText(df.format(new Date()));
+        habitDate.setText(df.format(habitEvent.getDate()));
 
-        // Todo: Change the icons' opacity according to the actual habit events' data from Firestore
-        locationIcon.setImageResource(R.drawable.ic_map_marker_positive);
-        descriptionIcon.setImageResource(R.drawable.ic_comment_negative);
-        ImageIcon.setImageResource(R.drawable.ic_image_positive);
+        // Set the location, comment, and image icons accordingly
+        if (isNull(habitEvent.getLocation())) {
+            locationIcon.setImageResource(R.drawable.ic_map_marker_negative);
+        } else {
+            locationIcon.setImageResource(R.drawable.ic_map_marker_positive);
+        }
+        if (isNull(habitEvent.getComment())) {
+            descriptionIcon.setImageResource(R.drawable.ic_comment_negative);
+        } else {
+            descriptionIcon.setImageResource(R.drawable.ic_comment_positive);
+        }
+        if (isNull(habitEvent.getImage())) {
+            ImageIcon.setImageResource(R.drawable.ic_image_negative);
+        } else {
+            ImageIcon.setImageResource(R.drawable.ic_image_positive);
+        }
 
         return view;
     }

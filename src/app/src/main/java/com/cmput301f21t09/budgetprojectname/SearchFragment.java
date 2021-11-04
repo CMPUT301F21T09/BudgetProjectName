c9851d906490f1ca6f24c5ddc722917c922fc2ca
Junesung Lee <junesoung412@gmail.com>
2021-11-04 14:20:28 -0600

    Add SearchFragment UI

--- src/app/src/main/java/com/cmput301f21t09/budgetprojectname/SearchFragment.java
+++ src/app/src/main/java/com/cmput301f21t09/budgetprojectname/SearchFragment.java
@@ -1,9 +1,13 @@
 package com.cmput301f21t09.budgetprojectname;
 
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.ImageButton;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -15,5 +19,34 @@ public class SearchFragment extends Fragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_search, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        ImageButton clearSearch = view.findViewById(R.id.clear_text);
+        EditText searchUser = view.findViewById(R.id.search_friend_edittext);
+        searchUser.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                clearSearch.setVisibility(View.VISIBLE);
+            }
+        });
+
+        clearSearch.setOnClickListener(v -> {
+            searchUser.getText().clear();
+            clearSearch.setVisibility(View.GONE);
+        });
+
     }
 }

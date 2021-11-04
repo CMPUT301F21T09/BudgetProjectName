bc3a39e8e427bf196371709bdcf315b95c94df40
cheetiantey <37930535+cheetiantey@users.noreply.github.com>
2021-11-04 15:06:48 -0600

    Revert "Delete habitevent (#132)"
    This reverts commit 6c65eb468cd3b1a2261fc0571cf47f4510abec68.

--- src/app/src/main/java/com/cmput301f21t09/budgetprojectname/HabitEventCustomList.java
+++ src/app/src/main/java/com/cmput301f21t09/budgetprojectname/HabitEventCustomList.java
@@ -1,9 +1,6 @@
 package com.cmput301f21t09.budgetprojectname;
 
-
-import static java.util.Objects.isNull;
 import static java.util.Objects.nonNull;
-
 
 import android.content.Context;
 import android.view.LayoutInflater;
@@ -44,13 +41,11 @@ public class HabitEventCustomList extends ArrayAdapter<HabitEventModel> {
 
         HabitEventModel habitEvent = habitEvents.get(position);
 
-
         /* Views */
 
         /**
          * Text View for habit date
          */
-
         TextView habitDate = view.findViewById(R.id.date);
 
         /**

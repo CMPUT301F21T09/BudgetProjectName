01a83ccb92a5f5cd93726fbbe882db722589c309
cheetiantey <37930535+cheetiantey@users.noreply.github.com>
2021-11-17 14:31:42 -0700

    Improve UI transitions of Delete Habit Event

--- src/app/src/main/java/com/cmput301f21t09/budgetprojectname/ViewHabitEventActivity.java
+++ src/app/src/main/java/com/cmput301f21t09/budgetprojectname/ViewHabitEventActivity.java
@@ -87,6 +87,7 @@ public class ViewHabitEventActivity extends AppCompatActivity {
             Intent deleteIntent = new Intent(getApplicationContext(), ViewHabitActivity.class);
             final String HABIT_ID = "HABIT_ID";
             deleteIntent.putExtra(HABIT_ID, habitID);
+            deleteIntent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
             startActivity(deleteIntent);
         });
     }

7fcde1bee3f71cbb7b594c8c85c19c91d44dbb93
Junesung Lee <junesoung412@gmail.com>
2021-11-17 17:34:51 -0700

    Comment

--- src/app/src/main/java/com/cmput301f21t09/budgetprojectname/UserCustomList.java
+++ src/app/src/main/java/com/cmput301f21t09/budgetprojectname/UserCustomList.java
@@ -5,21 +5,22 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.annotation.Nullable;
 
-import com.google.firebase.firestore.auth.User;
+import com.cmput301f21t09.budgetprojectname.models.UserModel;
 
 import java.util.ArrayList;
 
 /**
  * CustomList for CurrentUserProfileFragment and OtherUserProfileActivity
- * Show the habit's name, reason, and streak
+ * Show the user's name, username and profile photo
  */
-public class UserCustomList extends ArrayAdapter<User> {
-    private ArrayList<User> users;
+public class UserCustomList extends ArrayAdapter<UserModel> {
+    private ArrayList<UserModel> users;
     private Context context;
 
 
@@ -29,7 +30,7 @@ public class UserCustomList extends ArrayAdapter<User> {
      * @param context a current context of application
      * @param users   a arrayList of users
      */
-    public UserCustomList(Context context, ArrayList<User> users) {
+    public UserCustomList(Context context, ArrayList<UserModel> users) {
         super(context, 0, users);
         this.users = users;
         this.context = context;
@@ -40,11 +41,18 @@ public class UserCustomList extends ArrayAdapter<User> {
         if (view == null) {
             view = LayoutInflater.from(context).inflate(R.layout.user_custom_list, parent, false);
         }
-        User user = users.get(position);
+        UserModel user = users.get(position);
 
         ImageView profilePic = view.findViewById(R.id.profile_pic);
         TextView name = view.findViewById(R.id.name);
         TextView userName = view.findViewById(R.id.username);
+
+        Button follow = view.findViewById(R.id.following);
+        // if user following this user
+        // set follow button to gone or unfollow
+        follow.setOnClickListener(v -> {
+            // TODO: Request follow selected user
+        });
 
         return view;
     }
---
+++ src/app/src/main/java/com/cmput301f21t09/budgetprojectname/models/UserModel.java
@@ -0,0 +1,8 @@
+package com.cmput301f21t09.budgetprojectname.models;
+
+/**
+ * Model class for user habits
+ * PLACEHOLDER
+ */
+public class UserModel {
+}

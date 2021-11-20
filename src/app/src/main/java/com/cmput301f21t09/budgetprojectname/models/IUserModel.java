d92e4624b11c1f0803971c515621a6868f32cd5f
Mackenzie Malainey <macmalainey@gmail.com>
2021-11-19 22:11:04 -0700

    add usermodel

---
+++ src/app/src/main/java/com/cmput301f21t09/budgetprojectname/models/IUserModel.java
@@ -0,0 +1,10 @@
+package com.cmput301f21t09.budgetprojectname.models;
+
+public interface IUserModel {
+
+    String getUID();
+    String getUsername();
+    String getFirstName();
+    String getLastName();
+    
+}

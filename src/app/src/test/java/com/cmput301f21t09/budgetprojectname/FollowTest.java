718b79252d5adbbe1d0da1b22229ba835e15a01f
Justine Ventura <jvent@venturazure.onmicrosoft.com>
2021-11-27 12:55:56 -0700

    write tests

--- src/app/src/test/java/com/cmput301f21t09/budgetprojectname/FollowTest.java
+++ src/app/src/test/java/com/cmput301f21t09/budgetprojectname/FollowTest.java
@@ -1,11 +1,13 @@
 package com.cmput301f21t09.budgetprojectname;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotEquals;
+import static org.junit.Assert.assertNull;
 
-import com.cmput301f21t09.budgetprojectname.controllers.UserController;
 import com.cmput301f21t09.budgetprojectname.models.UserModel;
-import com.cmput301f21t09.budgetprojectname.services.AuthorizationService;
 
+
+import org.junit.Before;
 import org.junit.Test;
 
 import java.util.HashMap;
@@ -13,8 +15,24 @@ import java.util.HashMap;
 /**
  * Tests for operations involving follows, including following and requesting
  * to follow another user.
+ * Using Hashmap<String, Integer> to simulate Firebase DB functionality
  */
 public class FollowTest {
+
+    private String currentUserId;
+    private String anotherUserId;
+
+    /**
+     * Initializes test users
+     */
+    @Before
+    public void init() {
+        // using testuser1's uid for testing current user
+        currentUserId = "XPG70micV5XLyvhytqghBOLnK8U2";
+        // using testuser2's uid for testing another user
+        anotherUserId = "BtVfJ5exBBgpYXchbR4wsN8f0cp1";
+
+    }
 
     /**
      * Tests the sending a follow request
@@ -29,22 +47,18 @@ public class FollowTest {
      */
     @Test
     public void testReceiveRequest() {
-        // TODO: potentially move to before()
-        // using testuser1's uid for testing current user
-        String currentUserId = "XPG70micV5XLyvhytqghBOLnK8U2";
-        // using testuser2's uid for testing another user
-        String anotherUserId = "BtVfJ5exBBgpYXchbR4wsN8f0cp1";
-        UserController userController = new UserController();
-        userController.readUser(currentUserId, currentUserModel -> {
-                HashMap<String, Integer> socialMap = currentUserModel.getSocial();
-                // another user has sent current user a follow request
-                socialMap.put(anotherUserId, 0);
-                // check that this request is seen by current user
-                assertEquals(1, socialMap.size());
-                assertEquals(0,
-                        Integer.parseInt(String.valueOf(socialMap.get(anotherUserId))));
-        });
+        // current user
+        UserModel currentUser = new UserModel(currentUserId, "test1", "Test",
+                "User", new HashMap<String, Integer>());
+        HashMap<String, Integer> socialMap = currentUser.getSocial();
 
+        // another user has sent current user a follow request
+        socialMap.put(anotherUserId, 0);
+
+        // check that this request is seen by current user
+        assertEquals(1, socialMap.size());
+        assertEquals(0,
+                Integer.parseInt(String.valueOf(socialMap.get(anotherUserId))));
     }
 
     /**
@@ -52,7 +66,28 @@ public class FollowTest {
      */
     @Test
     public void testAcceptRequest() {
+        // current user
+        UserModel currentUser = new UserModel(currentUserId, "test1", "Test",
+                "User", new HashMap<String, Integer>());
+        HashMap<String, Integer> currentSocialMap = currentUser.getSocial();
 
+        // another user
+        UserModel anotherUser = new UserModel(anotherUserId, "test2", "Test2",
+                "User", new HashMap<String, Integer>());
+        HashMap<String, Integer> anotherSocialMap = anotherUser.getSocial();
+
+        // another user has sent current user a follow request
+        currentSocialMap.put(anotherUserId, 0);
+
+        // current user accepts follow request
+        currentSocialMap.remove(anotherUserId);
+        anotherSocialMap.put(currentUserId, 1);
+
+        // check that other user is removed from social map
+        assertEquals(0, currentSocialMap.size());
+        // check that other user is following current user (denoted by value 1 in social map)
+        assertEquals(1,
+                Integer.parseInt(String.valueOf(anotherSocialMap.get(currentUserId))));
     }
 
     /**
@@ -60,6 +95,26 @@ public class FollowTest {
      */
     @Test
     public void testDenyRequest() {
+        // current user
+        UserModel currentUser = new UserModel(currentUserId, "test1", "Test",
+                "User", new HashMap<String, Integer>());
+        HashMap<String, Integer> currentSocialMap = currentUser.getSocial();
+
+        // another user
+        UserModel anotherUser = new UserModel(anotherUserId, "test2", "Test2",
+                "User", new HashMap<String, Integer>());
+        HashMap<String, Integer> anotherSocialMap = anotherUser.getSocial();
+
+        // another user has sent current user a follow request
+        currentSocialMap.put(anotherUserId, 0);
+
+        // current user denies follow request
+        currentSocialMap.remove(anotherUserId);
+
+        // check that other user is removed from social map
+        assertEquals(0, currentSocialMap.size());
+        // check that other user is NOT following current user (denoted by value 1 in social map)
+        assertNull(anotherSocialMap.get(currentUserId));
 
     }
 

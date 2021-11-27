afeba49a8113378710a7cef110e052ea2f138f83
Justine Ventura <jvent@venturazure.onmicrosoft.com>
2021-11-26 23:35:34 -0700

    create template file for UT of follows

---
+++ src/app/src/test/java/com/cmput301f21t09/budgetprojectname/FollowTest.java
@@ -0,0 +1,58 @@
+package com.cmput301f21t09.budgetprojectname;
+
+import org.junit.Test;
+
+/**
+ * Tests for operations involving follows, including following and requesting
+ * to follow another user.
+ */
+public class FollowTest {
+
+    /**
+     * Tests the sending a follow request
+     */
+    @Test
+    public void testSendRequest(){
+
+    }
+
+    /**
+     * Tests the receiving a follow request
+     */
+    @Test
+    public void testReceiveRequest(){
+
+    }
+
+    /**
+     * Tests the accepting of a follow request
+     */
+    @Test
+    public void testAcceptRequest(){
+
+    }
+
+    /**
+     * Tests the denial of a follow request
+     */
+    @Test
+    public void testDenyRequest(){
+
+    }
+
+    /**
+     * Tests when another user has accepted request
+     */
+    @Test
+    public void testRequestIsAccepted(){
+
+    }
+
+    /**
+     * Tests when another user has denied request
+     */
+    @Test
+    public void testRequestIsDenied(){
+
+    }
+}
